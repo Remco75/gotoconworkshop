0a47a87db09146d125bc5a67aa3bd2f3ae9cd6ec
remco vlierman <remco@remcos-MacBook-Pro.local>
2015-05-31 19:56:35 +0200

    scaffolding with yeoman and scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gotoconSliderApp
+ * @description
+ * # gotoconSliderApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gotoconSliderApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gotoconSliderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gotoconSliderApp
+ */
+angular.module('gotoconSliderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
